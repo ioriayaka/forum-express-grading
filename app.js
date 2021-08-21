1a7c8f0e220c326c4b6a29491890bd0faeb18338
ioriayaka <ioriayaka@hotmail.com>
2021-08-21 23:20:42 +0800

    Add userController and signup page Confirm user password and unique email

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const handlebars = require('express-handlebars') // 引入 handlebars
 const db = require('./models') // 引入資料庫
+const bodyParser = require('body-parser')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = 3000
 // 設定 view engine 使用 handlebars
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }  )) // Handlebars 註冊樣板引擎
 app.set('view engine', 'handlebars') // 設定使用 Handlebars 做為樣板引擎
-
+app.use(bodyParser.urlencoded({ extended: true }))
+// setup handlebars
+// setup bodyParser
+// setup session and flash
+app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+app.use(flash())
+// 把 req.flash 放到 res.locals 裡面
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
