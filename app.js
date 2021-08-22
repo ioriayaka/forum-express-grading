1f661daf5e10d5f9f61d2994f4f00e9502393309
ioriayaka <ioriayaka@hotmail.com>
2021-08-22 10:50:48 +0800

    Install passport and passport-local Add feature: signInPage, signIn and logout

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const db = require('./models') // 引入資料庫
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const app = express()
 const port = 3000
 // 設定 view engine 使用 handlebars
@@ -14,6 +15,9 @@ app.use(bodyParser.urlencoded({ extended: true }))
 // setup bodyParser
 // setup session and flash
 app.use(session({ secret: 'secret', resave: false, saveUninitialized: false }))
+// setup passport
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 // 把 req.flash 放到 res.locals 裡面
 app.use((req, res, next) => {
@@ -25,6 +29,6 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 // 引入 routes 並將 app 傳進去，讓 routes 可以用 app 這個物件來指定路由
-require('./routes')(app)
+require('./routes')(app, passport) // 把 passport 傳入 routes
 
 module.exports = app
